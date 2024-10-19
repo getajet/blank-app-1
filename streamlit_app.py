59505b1ef24ccd16208d9a3476bb2c7d3c20dba5
getajet <cloth@duckspam.com>
2024-10-19 23:28:04 +0000

    workin!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,30 @@
 import streamlit as st
+import pty
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+def run_app():
+    # Create a pseudoterminal and attach it to the command
+    def spawn_and_read():
+        def read(fd):
+            while True:
+                output = os.read(fd, 1024).decode()
+                if output:
+                    st.session_state.console_output += output
+
+        pty.spawn(['./sshx'], read)
+
+    spawn_and_read()
+
+# Streamlit UI
+st.title("Console Output")
+
+if "console_output" not in st.session_state:
+    st.session_state.console_output = ""
+
+# Button to start the application
+if st.button("Start App"):
+    run_app()
+
+# Display console output
+st.text_area("Console", value=st.session_state.console_output, height=400, max_chars=None)
